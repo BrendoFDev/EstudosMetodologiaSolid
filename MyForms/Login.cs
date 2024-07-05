bf62bd6b9fc6b1692b07a0820a11da5fc01c1d5e
BrendoFDev <brendo.dkry@gmail.com>
2024-07-04 22:22:20 -0300

    estudos com windows forms

---
+++ MyForms/Login.cs
@@ -0,0 +1,29 @@
+namespace MyForms
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void txtName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                txtPassword.Focus();
+        }
+
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            EventHandler eventhandler = new EventHandler(bttLogin_Click);
+            if (e.KeyCode == Keys.Enter)
+                eventhandler(null,e);
+        }
+
+        private void bttLogin_Click(object? sender, EventArgs e)
+        {
+
+        }
+    }
+}
